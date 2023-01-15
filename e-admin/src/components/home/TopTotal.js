9ae18c3456515f871e3b8bc97cb79fc4f962fa27
Samuel Mintah <samiblaq8@gmail.com>
2023-01-14 23:45:28 -0800

    toptotal

--- e-admin/src/components/home/TopTotal.js
+++ e-admin/src/components/home/TopTotal.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const TopTotal = () => {
   return (
-    <div>TopTotal</div>
-  )
+    <div className="bg-white p-6 rounded-lg shadow">
+      <div className="text-lg font-medium">Sales</div>
+      <div className="text-4xl font-medium text-indigo-600">
+        {/* ${sales} */}
+      </div>
+    </div>
+)
 }
+
 
 export default TopTotal
