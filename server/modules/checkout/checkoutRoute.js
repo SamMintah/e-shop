0124feca42cbba5d31ee4d4e1941bd29b630da72
Samuel Mintah <samiblaq8@gmail.com>
2023-02-25 23:17:00 +0000

    Moved server to separate repo for better organization

--- server/modules/checkout/checkoutRoute.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router;
-const { createCheckout } = require("./checkoutcontroller");
-
-const checkoutRouter = router();
-
-checkoutRouter.route("/checkout").post(createCheckout);
-
-module.exports = { checkoutRouter };
